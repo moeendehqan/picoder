b2e7f81eb6920dda9df83b75cf59eb6099c1952f
moeen dehqan <moeen.dehqan@gmail.com>
2023-09-21 13:44:16 +0330

    .

--- src/App.js
+++ src/App.js
@@ -3,16 +3,45 @@ import { useState } from 'react';
 function App() {
 
   const [image,setImage] = useState(null)
+  const [Char,setChar] = useState(['@','#','!','$','%','^','&','*','-','_','+','=','~','<','>','O','0','X',';',':','?','"','I','A','V','T','W','M'])
+  const [CharSelected, setCharSelected] = useState([])
+  const [Size, setSize] = useState({w})
+
+
+  const handleCharSelected = (character) => {
+    const isItemInList = CharSelected.includes(character);
+    if (isItemInList) {
+      const updatedList = CharSelected.filter((listItem) => listItem !== character);
+      setCharSelected(updatedList);
+    } else {
+      const updatedList = [...CharSelected, character];
+      setCharSelected(updatedList);
+    }
+  }
+  
 
   return (
     <div>
       <div className='img'>
-        <input type='file' value={image} onChange={(e)=>setImage(e.target.files[0])}/>
-        
-
-
+        <input type="file" accept="image/*" onChange={(e) => setImage(e.target.files[0])} />
+        {image && <img src={URL.createObjectURL(image)} alt='تصویر انتخابی' />}
+      </div>
+      <div className='chr'>
+        {
+          Char.map(i=>{
+            return(
+              <div onClick={()=>handleCharSelected(i)} className='ChrBox'>
+                <p>{i}</p>
+              </div>
+            )
+          })
+        }
       </div>
 
+      <div className='siz'>
+        <input  />
+
+      </div>
     </div>
   );
 }
