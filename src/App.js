99cf5eb033e6ba38b166b3f9c469bd33843cbe35
moeen dehqan <moeen.dehqan@gmail.com>
2023-09-21 12:32:41 +0330

    picoer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './style/style.css'
+import { useState } from 'react';
 function App() {
+
+  const [image,setImage] = useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='img'>
+        <input type='file' value={image} onChange={(e)=>setImage(e.target.files[0])}/>
+        
+
+
+      </div>
+
     </div>
   );
 }
